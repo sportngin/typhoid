35857a488aeb063fd5020c41aa3e16f7eee48f81
Jon Phenow <j.phenow@gmail.com>
2013-06-07 13:33:41 -0500

    pull in new objects for decorating

--- lib/typhoid.rb
+++ lib/typhoid.rb
@@ -2,6 +2,14 @@ require "typhoid/version"
 require 'typhoid/uri'
 require 'typhoid/parser'
 require 'typhoid/builder'
+
+module Typhoid
+  TyphoeusCompatabilityError = Class.new StandardError
+end
+
+require 'typhoid/typhoeus_decorator'
+require 'typhoid/request'
+require 'typhoid/response'
 require "typhoid/request_queue"
 require "typhoid/queued_request"
 require "typhoid/multi"
