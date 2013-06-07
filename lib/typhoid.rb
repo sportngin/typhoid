193eaaa982487f816950a88cd2feb2605aafc058
Jon Phenow <j.phenow@gmail.com>
2013-06-07 15:05:07 -0500

    provide typhoeus version helpers in typhoid

--- lib/typhoid.rb
+++ lib/typhoid.rb
@@ -5,6 +5,32 @@ require 'typhoid/builder'
 
 module Typhoid
   TyphoeusCompatabilityError = Class.new StandardError
+
+  def self.typhoeus
+    @typhoeus ||= TyphoeusDescriptor.new
+  end
+
+  class TyphoeusDescriptor
+    def version
+      Typhoeus::VERSION
+    end
+
+    def matched_version
+      version.to_s.match /(?<major>\d+)\.(?<minor>\d+)\.(?<bug>\d+)/
+    end
+
+    def major_version
+      matched_version[:major].to_i
+    end
+
+    def minor_version
+      matched_version[:minor].to_i
+    end
+
+    def bug_version
+      matched_version[:bug].to_i
+    end
+  end
 end
 
 require 'typhoid/typhoeus_decorator'
