2a08d3f644b2f2505509eca8053e63ec55163cb5
Jon Phenow <j.phenow@gmail.com>
2013-06-07 15:06:43 -0500

    use newly named class helper #source_class

--- lib/typhoid/response.rb
+++ lib/typhoid/response.rb
@@ -3,7 +3,7 @@ module Typhoid
   class Response < TyphoeusDecorator
     decorate ::Typhoeus::Response
 
-    klass_to_decorate.instance_methods.map { |m| m.to_s.match(/response\_(?<name>\w*)/) }.compact.each do |match|
+    source_klass.instance_methods.map { |m| m.to_s.match(/response\_(?<name>\w*)/) }.compact.each do |match|
       define_method match[:name] do
         source.public_send("response_#{match[:name]}")
       end
