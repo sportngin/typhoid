cef44d4a9132ef5c54d49e9ad3dcf97d7e2160df
Jon Phenow <j.phenow@gmail.com>
2013-06-11 15:17:57 -0500

    backport options method

--- lib/typhoid/request.rb
+++ lib/typhoid/request.rb
@@ -3,6 +3,41 @@ module Typhoid
   class Request < TyphoeusDecorator
     decorate ::Typhoeus::Request
 
+    ACCESSOR_OPTIONS = [
+      :method,
+      :params,
+      :body,
+      :headers,
+      :cache_key_basis,
+      :connect_timeout,
+      :timeout,
+      :user_agent,
+      :response,
+      :cache_timeout,
+      :follow_location,
+      :max_redirects,
+      :proxy,
+      :proxy_username,
+      :proxy_password,
+      :disable_ssl_peer_verification,
+      :disable_ssl_host_verification,
+      :interface,
+      :ssl_cert,
+      :ssl_cert_type,
+      :ssl_key,
+      :ssl_key_type,
+      :ssl_key_password,
+      :ssl_cacert,
+      :ssl_capath,
+      :ssl_version,
+      :verbose,
+      :username,
+      :password,
+      :auth_method,
+      :proxy_auth_method,
+      :proxy_type
+    ]
+
     def run
       if Typhoid.typhoeus.major_version == 0
         if Typhoid.typhoeus.minor_version >= 6
@@ -21,6 +56,13 @@ module Typhoid
     # Need to force override, because Object#method
     def method
       source.respond_to?(:method) && source.method || options[:method] || :get
+    end
+
+    def options
+      source.respond_to?(:options) || ACCESSORS.reduce({}) do |hash, key|
+        hash[key] = source.send(key) if source.respond_to?(:key)
+        hash
+      end
     end
   end
 end
