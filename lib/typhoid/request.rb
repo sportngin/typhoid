a25fd6b7394e84935b48c8aa2349fa96e9074ece
Jon Phenow <j.phenow@gmail.com>
2013-06-11 14:59:16 -0500

    fix compatability

--- lib/typhoid/request.rb
+++ lib/typhoid/request.rb
@@ -4,7 +4,14 @@ module Typhoid
     decorate ::Typhoeus::Request
 
     def run
-      Typhoid::Response.new source.run
+      if Typhoid.typhoeus.major_version == 0
+        if Typhoid.typhoeus.minor_version <= 6
+          response = source.run
+        else
+          response = Typhoeus::Request.send(method, url, options)
+        end
+      end
+      Typhoid::Response.new response
     end
 
     def response
@@ -13,7 +20,7 @@ module Typhoid
 
     # Need to force override, because Object#method
     def method
-      source.method
+      source.respond_to?(:method) && source.method || options[:method] || :get
     end
   end
 end
